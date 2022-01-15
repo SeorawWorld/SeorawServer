7a6f36cbba04a148d6314f905c1592b64675c596
Seoraw <97466813+Seoraw@users.noreply.github.com>
2022-01-15 16:01:36 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,19 @@
 @echo off
 :start
 title Seoraw
+
 del /Q logs\*.*
+
 git fetch --all
 git reset --hard origin/master
 git pull
+
+del /Q backup\*.*
+mkdir backup
+"C:\Program Files\WinRAR\WinRAR.exe" a backup\world.rar -o world
+"C:\Program Files\WinRAR\WinRAR.exe" a backup\world_nether.rar -o world_nether
+"C:\Program Files\WinRAR\WinRAR.exe" a backup\world_the_end.rar -o world_the_end
+
 java -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar paper.jar -nogui
 cls
 goto :start
