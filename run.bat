5905ae157dc776ddb1da7e5580ba588a515aeacc
Seoraw <97466813+Seoraw@users.noreply.github.com>
2022-01-16 14:04:08 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -14,7 +14,7 @@ mkdir backup
 "C:\Program Files\WinRAR\WinRAR.exe" a backup\world_nether.rar -o world_nether
 "C:\Program Files\WinRAR\WinRAR.exe" a backup\world_the_end.rar -o world_the_end
 
-java -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar paper.jar -nogui
+"..\zulu17\bin\java.exe" -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar paper.jar -nogui
 cls
 goto :start
 pause
